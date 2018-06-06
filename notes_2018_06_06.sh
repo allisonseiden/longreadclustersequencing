ceff57d0731afdd6c902212a18ea107649f4da89
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-06 14:40:39 -0400

    added whatshap commands to today's notes

--- notes_2018_06_06.sh
+++ notes_2018_06_06.sh
@@ -5,4 +5,15 @@ module load python/3.5.0
 module load py_packages/3.5
 module load tabix
 
- virtualenv myvenv
+# starting the virtual environment
+virtualenv myvenv
+source myvenv/bin/activate
+
+
+# installing whatshap
+module purge
+module load python/3.5.0 py_packages/3.5 tabix
+virtualenv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install whatshap
