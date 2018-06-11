9a5b620ce11220a4c24a5731a89cea7fa084c2ef
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-11 15:25:06 -0400

    modified today's notes

--- notes_2018_06_11.sh
+++ notes_2018_06_11.sh
@@ -1,2 +1,8 @@
 # samtools adding read groups to files
-samtools addreplacerg -r ID:ID number.1 -r LIB:hg38 -r SM:ID -o ID_edit.bam <bam_file> 
+samtools addreplacerg -r ID:ID number.1 -r LIB:hg38 -r SM:ID -o ID_edit.bam <bam_file>
+
+# samtools making sure files are same size
+samtools view file_name | wc -l
+
+# make index files with python script, use subprocess? before calling whatshap
+# for every individual, make their index file first
