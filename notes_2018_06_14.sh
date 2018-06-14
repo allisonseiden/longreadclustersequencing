251117e91b564b21421c87046e87e929384564da
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-14 10:38:54 -0400

    adding today's notes and IGV snapshots

---
+++ notes_2018_06_14.sh
@@ -0,0 +1,5 @@
+# command I used to run whatshap (working so far)
+whatshap phase --ped test.ped --indels --reference
+/sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa --distrust-genotypes --include-homozygous -o
+phasedFile.vcf ~/www/PacbioProject/DNV_calls/VCF/TrioVCF/ID ~/www/PacbioProject/PacbioHg38Bams/ID_edit_sorted
+~/www/PacbioProject/IlluminaHg38Bams/ID-mom ~/www/PacbioProject/IlluminaHg38Bams/ID-dad
