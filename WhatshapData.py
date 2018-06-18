0c52cad0452590798f24246847d6e815fe43b5e1
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-18 10:33:23 -0400

    edits to cluster analysis class

--- WhatshapData.py
+++ WhatshapData.py
@@ -2,17 +2,18 @@
 # PacBio Project, creating class for Whatshap data to run shell commands
 
 class WhatshapData(object):
-    def __init__(self, patientID, phaseFileName, inputVCF, inputBAM):
+    def __init__(self, patientID, pedFileName, phaseFileName, inputVCF, inputBAM):
         self.patient = patientID;
+        self.ped = pedFileName;
         self.phasedName = phaseFileName;
         self.variantFileName = inputVCF;
         self.sequenceFileName = inputBAM;
         self.command = "";
 
     def cmd(self):
-        self.command = "whatshap phase --ignore-read-groups --distrust-genotypes";
-        self.command += " --indels --reference (insert ref file here)"
-        self.command += " --include-homozygous -o ${output}" + self.phasedName;
-        self.command += " ${output}" + self.variantFileName;
-        self.command += " ${output}" + self.sequenceFileName;
+        self.command = "whatshap phase --ped " + self. ped + "--ignore-read-groups";
+        self.command += " --distrust-genotypes --indels";
+        self.command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
+        self.command += " --include-homozygous -o " + self.phasedName;
+        self.command += self.variantFileName + self.sequenceFileName;
         return self.command;
