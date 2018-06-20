477f71672fec60daf477edb9193cd9cefbdc661b
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-20 12:04:43 -0400

    edits:

--- run_whatshap.py
+++ run_whatshap.py
@@ -1,17 +1,25 @@
 import subprocess
+from WhatshapData import WhatshapData
+import multiprocessing as mp
 
 patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
-for ID in patientID:
+#for ID in patientID:
+def whatshap(ID):
     pedFileName = "hpc/users/seidea02/longreadclustersequencing/data/" + ID + ".ped";
-    child_input = "hpc/users/seidea02/www/PacbioProject/PacbioHg38Bams/" + ID + "_edit_sorted.bam";
-    dad_input = "hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-02.hg38.dedup.clean.recal.cram";
-    mom_input = "hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-01.hg38.dedup.clean.recal.cram";
+    child_input = "hpc/users/seidea02/www/PacbioProject/PacbioHg38Bams/" + ID + "_edit_sorted.bam ";
+    dad_input = "hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-02.hg38.dedup.clean.recal.cram ";
+    mom_input = "hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-01.hg38.dedup.clean.recal.cram ";
     for i in range(1, 23):
-        phaseFileName = ID + "_chr" + str(i) + "_phased.vcf";
-        inputVCF = "hpc/users/seidea02/www/PacbioProject/DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr" + str(i) + ".vcf.gz";
+        phaseFileName = ID + "_chr" + str(i) + "_phased.vcf ";
+        inputVCF = "hpc/users/seidea02/www/PacbioProject/DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr" + str(i) + ".vcf.gz ";
         whatshap_object = WhatshapData(ID, pedFileName, phaseFileName, inputVCF, child_input, dad_input, mom_input);
-        print("========WhatshapData object created for patient family " + ID + " for chromosome " + str(i));
+        print("========WhatshapData object created for patient family " + ID + " on chromosome " + str(i));
         command = whatshap_object.cmd();
         print("--------Running whatshap for patient family " + ID + " on chromosome " + str(i));
         subprocess.call(command, shell=True);
+        print("********Succesfully ran whatshap for patient family " + ID + " on chromosome " + str(i));
+
+if __name__ == '__main__':
+    pool = mp.Pool(processes=6);
+    pool.map(whatshap, patientID[1]);
