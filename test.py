7883812044b0c874ae70098576ad2beb01041d89
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-20 09:19:16 -0400

    edit to test subprocess

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
 
 for i in range(19, 20):
-    command = "tabix -h ../1-00801.hg38.trio.vcf.gz chr" + i + " > 1-00801_chr" + i + ".vcf";
+    command = "tabix -h ../1-00801.hg38.trio.vcf.gz chr" + str(i) + " > 1-00801_chr" + str(i) + ".vcf";
     subprocess.call(command, shell=True);
