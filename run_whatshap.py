43eba405ce914aba58390434b7924b0ad898e8b9
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-21 09:44:09 -0400

    edits

--- run_whatshap.py
+++ run_whatshap.py
@@ -11,14 +11,15 @@ def whatshap(ID):
     dad_input = "/hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-02.hg38.dedup.clean.recal.cram ";
     mom_input = "/hpc/users/seidea02/www/PacbioProject/IlluminaHg38Bams/" + ID + "-01.hg38.dedup.clean.recal.cram ";
     for i in range(1, 23):
-        phaseFileName = ID + "_chr" + str(i) + "_phased.vcf ";
-        inputVCF = "/hpc/users/seidea02/www/PacbioProject/DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr" + str(i) + ".vcf.gz ";
+        chrom = str(i);
+        phaseFileName = ID + "_chr" + chrom + "_phased.vcf ";
+        inputVCF = "/hpc/users/seidea02/www/PacbioProject/DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr" + chrom + ".vcf.gz ";
         whatshap_object = WhatshapData(ID, pedFileName, phaseFileName, inputVCF, child_input, dad_input, mom_input);
-        print("========WhatshapData object created for patient family " + ID + " on chromosome " + str(i));
+        print("========WhatshapData object created for patient family " + ID + " on chromosome " + chrom);
         command = whatshap_object.cmd();
-        print("--------Running whatshap for patient family " + ID + " on chromosome " + str(i));
+        print("--------Running whatshap for patient family " + ID + " on chromosome " + chrom);
         subprocess.call(command, shell=True);
-        print("********Succesfully ran whatshap for patient family " + ID + " on chromosome " + str(i));
+        print("********Succesfully ran whatshap for patient family " + ID + " on chromosome " + chrom);
 
 if __name__ == '__main__':
     pool = mp.Pool(processes=6);
