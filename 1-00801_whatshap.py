70cdc97769d3fe49055e814f846c11b468386bb3
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-22 11:52:35 -0400

    needed to add spaces to command

--- 1-00801_whatshap.py
+++ 1-00801_whatshap.py
@@ -6,8 +6,8 @@ for i in range(1,16):
     bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/
     bam_filename += "PacbioHg38Bams/1-00801_edit_sorted.bam"
     command = "whatshap phase --sample=1-00801 --ignore-read-groups";
-    command += "--reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
-    command += "--indels -o 1-00801/1-00801_chr" + str(i) + "_phased.vcf ";
+    command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
+    command += " --indels -o 1-00801/1-00801_chr" + str(i) + "_phased.vcf ";
     command += vcf_filename + " " + bam_filename;
     sp.call(command, shell=True);
     print("======Sucessfully ran whatshap for 1-00801 on chromosome " + str(i));
