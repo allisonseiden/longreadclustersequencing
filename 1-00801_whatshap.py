b758d51b54e8aa1c78271fc3606c7e7624e2bfa9
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-22 11:53:52 -0400

    fixed error with quotes

--- 1-00801_whatshap.py
+++ 1-00801_whatshap.py
@@ -1,10 +1,10 @@
 import subprocess as sp
 
 for i in range(1,16):
-    vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/"
-    vcf_filename += "DNV_calls/VCF/TrioVCF/1-00801/1-00801_chr" + str(i) + ".vcf.gz"
-    bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/
-    bam_filename += "PacbioHg38Bams/1-00801_edit_sorted.bam"
+    vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
+    vcf_filename += "DNV_calls/VCF/TrioVCF/1-00801/1-00801_chr" + str(i) + ".vcf.gz";
+    bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
+    bam_filename += "PacbioHg38Bams/1-00801_edit_sorted.bam";
     command = "whatshap phase --sample=1-00801 --ignore-read-groups";
     command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
     command += " --indels -o 1-00801/1-00801_chr" + str(i) + "_phased.vcf ";
