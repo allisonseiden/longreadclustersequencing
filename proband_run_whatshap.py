263c6e526be4605822248e945453174462b4c0ac
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-25 09:55:48 -0400

    removed one proband from list that was run individually

--- proband_run_whatshap.py
+++ proband_run_whatshap.py
@@ -1,6 +1,6 @@
 import subprocess as sp
 
-patientID = ["1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
+patientID = ["1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
 vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
 bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
