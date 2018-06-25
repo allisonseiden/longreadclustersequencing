0403cc2a565a7787566c6f569553ac9710519c4d
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-25 10:34:00 -0400

    edit to script to have correct file names

--- proband_run_whatshap.py
+++ proband_run_whatshap.py
@@ -2,13 +2,14 @@ import subprocess as sp
 
 patientID = ["1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
-vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
 for ID in patientID:
-    vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
-    bam_filename += ID + "_edit_sorted.bam";
+
     for i in range(1,23):
+        vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
+        vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
         vcf_filename += str(i) + ".vcf.gz"
+        bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
+        bam_filename += ID + "_edit_sorted.bam";
         command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
         command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
         command += " --indels -o " + ID + "/" + ID + "_chr" + str(i) + "_phased.vcf ";
