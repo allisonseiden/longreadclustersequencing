f183bf99263cf58c1a1faacc70d7d7ddea20d2a9
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-28 10:20:36 -0400

    attempting pandas to check for discontinuities in phased VCFs

---
+++ check_discontinuities.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+chr22_100801 = pd.read_table('/hpc/users/seidea02/www/PacbioProject/WhatshapVCFs/1-00801/1-00801_chr22_phased.vcf',
+                                columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', '1-00801', '1-00801-01', '1-00801-02']);
+
+print(chr22_100801.loc[:,'CHROM']);
