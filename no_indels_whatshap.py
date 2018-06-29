917815f9bef5c0fc047f31e28bcc4c8e7fe21cfc
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-29 12:35:23 -0400

    fixed multiprocessing error

--- no_indels_whatshap.py
+++ no_indels_whatshap.py
@@ -4,19 +4,18 @@ import multiprocessing as mp
 patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
 def whatshap(ID):
-  for ID in patientID:
-      for i in range(1,23):
-          vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-          vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
-          vcf_filename += str(i) + ".vcf.gz"
-          bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
-          bam_filename += ID + "_edit_sorted.bam";
-          command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
-          command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
-          command += " -o " + ID + "_no_indels/" + ID + "_chr" + str(i) + "_phased.vcf ";
-          command += vcf_filename + " " + bam_filename;
-          sp.call(command, shell=True);
-          print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
+  for i in range(1,23):
+      vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
+      vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
+      vcf_filename += str(i) + ".vcf.gz"
+      bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
+      bam_filename += ID + "_edit_sorted.bam";
+      command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
+      command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
+      command += " -o " + ID + "_no_indels/" + ID + "_chr" + str(i) + "_phased.vcf ";
+      command += vcf_filename + " " + bam_filename;
+      sp.call(command, shell=True);
+      print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
 
 if __name__ == '__main__':
   pool = mp.Pool(processes=5);
