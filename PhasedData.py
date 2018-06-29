12f66f9dd7f08ce1d879bc4c1f5bd5beb407600d
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-29 15:09:22 -0400

    error handling

--- PhasedData.py
+++ PhasedData.py
@@ -45,26 +45,22 @@ class PhasedData:
 
     def search_discon(self, chromosome):
         chr_bounds = {};
-        print('current chromosome: ');
-        print(chromosome);
         curr_vcf = self.vcf_dfs[chromosome];
-        print('list of de novos for current chromosome: ');
-        print(self.dnvs[chromosome]);
-            # #for dnv in self.dnvs[chr]:
-            #     chr_bounds[dnv] = [];
-            #     dnv_index = curr_vcf.index[curr_vcf['POS'] == dnv].item();
-            #     hap = curr_vcf[self.id][dnv_index];
-            #     u_discon= dnv_index;
-            #     while hap[:3] != "0/1" and hap[:3] != "1/0":
-            #         u_discon -= 1;
-            #         hap = curr_vcf[self.id][u_discon];
-            #     chr_bounds[dnv].append(curr_vcf['POS'][u_discon]);
-            #     hap = curr_vcf[self.id][dnv_index];
-            #     l_discon = dnv_index;
-            #     while hap[:3] != "0/1" and hap[:3] != "1/0":
-            #         l_discon += 1;
-            #         hap = curr_vcf[self.id][l_discon];
-            #     chr_bounds[dnv].append(curr_vcf['POS'][l_discon]);
+        for dnv in self.dnvs[chromosome]:
+            chr_bounds[dnv] = [];
+            dnv_index = curr_vcf.index[curr_vcf['POS'] == dnv].item();
+            hap = curr_vcf[self.id][dnv_index];
+            u_discon= dnv_index;
+            while hap[:3] != "0/1" and hap[:3] != "1/0":
+                u_discon -= 1;
+                hap = curr_vcf[self.id][u_discon];
+            chr_bounds[dnv].append(curr_vcf['POS'][u_discon]);
+            hap = curr_vcf[self.id][dnv_index];
+            l_discon = dnv_index;
+            while hap[:3] != "0/1" and hap[:3] != "1/0":
+                l_discon += 1;
+                hap = curr_vcf[self.id][l_discon];
+            chr_bounds[dnv].append(curr_vcf['POS'][l_discon]);
         return chr_bounds;
 
     def fill_bounds_dictionary(self):
