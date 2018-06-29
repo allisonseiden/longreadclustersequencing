6946fd64388f63856af233d79bdd0075c53e5c27
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-06-29 15:22:41 -0400

    error handling

--- PhasedData.py
+++ PhasedData.py
@@ -21,8 +21,8 @@ class PhasedData:
             self.vcf_dfs["chr{0}".format(i)] = pd.read_table('/hpc/users/seidea02/www/PacbioProject/WhatshapVCFs/' + self.id + '/' + self.id + '_chr' + num + '_phased.vcf',
                                             sep='\t', names = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', '1-00801', '1-00801-01', '1-00801-02'],
                                             comment = '#');
-        for keys in self.vcf_dfs:
-            print(keys);
+        # for keys in self.vcf_dfs:
+        #     print(keys);
 
         #print('------------End of vcf dictionary keys');
 
@@ -67,38 +67,44 @@ class PhasedData:
         for chr in self.dnvs:
             self.bounds[chr] = self.search_discon(chr);
 
-    def find_variants_for_phasing(self):
-        for chr in self.bounds:
-            curr_vcf = self.vcf_dfs[chr];
-            for dnv in self.bounds[chr]:
+    def find_variants_for_phasing_chr(self, chromosome):
+        #for chr in self.bounds:
+        chr_phase = {};
+        curr_vcf = self.vcf_dfs[chromosome];
+        for dnv in self.bounds[chromosome]:
                 # de novo is dnv
                 # list of bounds for de novo is all_bounds[chr][dnv]
-                curr_bounds = self.bounds[chr][dnv];
-                self.to_phase[dnv] = [];
-                u_index = curr_vcf.index[curr_vcf['POS'] == curr_bounds[0]].item();
+            curr_bounds = self.bounds[chromosome][dnv];
+            chr_phase[dnv] = [];
+            u_index = curr_vcf.index[curr_vcf['POS'] == curr_bounds[0]].item();
                 #print(u_index_list);
                 #u_index = u_index_list[0];
-                l_index = curr_vcf.index[curr_vcf['POS'] == curr_bounds[1]].item();
+            l_index = curr_vcf.index[curr_vcf['POS'] == curr_bounds[1]].item();
                 #l_index = l_index_list[0];
-                position = u_index;
-                while position <= l_index:
-                    child = curr_vcf[self.id][position];
-                    mom = curr_vcf[self.mom][position];
-                    dad = curr_vcf[self.dad][position];
-                    if child[:3] == "0|1" or child[:3] == "1|0":
-                        if mom[:3] == "0/0" and (dad[:3] == "1/1" or dad[:3] == "0/1"):
-                            self.to_phase[dnv].append(curr_vcf['POS'][position]);
-                            if dad[:3] == "0/0" and (mom[:3] == "1/1" or mom[:3] == "0/1"):
-                                self.to_phase[dnv].append(curr_vcf['POS'][position]);
-                    position += 1;
+            position = u_index;
+            while position <= l_index:
+                child = curr_vcf[self.id][position];
+                mom = curr_vcf[self.mom][position];
+                dad = curr_vcf[self.dad][position];
+                if child[:3] == "0|1" or child[:3] == "1|0":
+                    if mom[:3] == "0/0" and (dad[:3] == "1/1" or dad[:3] == "0/1"):
+                        chr_phase[dnv].append(curr_vcf['POS'][position]);
+                        if dad[:3] == "0/0" and (mom[:3] == "1/1" or mom[:3] == "0/1"):
+                            chr_phase[dnv].append(curr_vcf['POS'][position]);
+                position += 1;
+        return chr_phase;
+
+    def find_variants_for_phasing(self):
+        for chr in self.bounds:
+            self.to_phase = self.find_variants_for_phasing_chr(chr);
 
     def assign_to_parent(self, chromosome):
         curr_vcf = self.vcf_dfs[chromosome];
-        for dnv in self.to_phase:
+        for dnv in self.to_phase[chromosome]:
             self.phased_to_parent[dnv] = [];
             dnv_index = curr_vcf.index[curr_vcf['POS'] == dnv].item();
             de_novo_hap = curr_vcf[self.id][dnv_index];
-            for var in self.to_phase[dnv]:
+            for var in self.to_phase[chromosome][dnv]:
                 index = curr_vcf.index[curr_vcf['POS'] == var].item();
                 if len(curr_vcf['REF'][index]) > 1 or len(curr_vcf['ALT'][index]) > 1:
                     print("Skipped variant at position " + str(vcf_df['POS'][index]));
