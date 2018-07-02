e741a8864905c77a6a509d1f8949d84dc2b89c3b
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-02 16:03:12 -0400

    trying multiprocessing

--- phase_to_parent.py
+++ phase_to_parent.py
@@ -3,21 +3,38 @@ import pandas as pd;
 import numpy as np;
 
 patientIDs = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460",
-                "1-04537", "1-05443", "1-05673", "1-05846"];
+                 "1-04537", "1-05443", "1-05673", "1-05846"];
 
 phased_data_objects = [];
 complete_df = pd.DataFrame();
 
-for ID in patientIDs:
-    phased_data_objects.append(PhasedData(ID));
+# for ID in patientIDs:
+def run_phase_to_parent(ID):
+    patient = PhasedData(ID);
+    patient.create_vcf_dictionary();
+    patient.create_dnvs_dictionary();
+    patient.fill_bounds_dictionary();
+    patient.find_variants_for_phasing();
+    patient.assign_to_parent();
+    patient.convert_to_dataframe();
+    phased_data_objects.append(patient);
 
-for patient_object in phased_data_objects:
-    patient_object.create_vcf_dictionary();
-    patient_object.create_dnvs_dictionary();
-    patient_object.fill_bounds_dictionary();
-    patient_object.find_variants_for_phasing();
-    patient_object.assign_to_parent();
-    patient_object.convert_to_dataframe();
-    complete_df.append(patient_object.parent_df);
+if __name__ == '__main__':
+    pool = mp.Pool(processes=5);
+    pool.map(run_phase_to_parent, patientIDs);
 
-print(complete_df);
+    for patient in phased_data_objects:
+        complete_df.append(patient.parent_df);
+
+
+
+# for patient_object in phased_data_objects:
+#     patient_object.create_vcf_dictionary();
+#     patient_object.create_dnvs_dictionary();
+#     patient_object.fill_bounds_dictionary();
+#     patient_object.find_variants_for_phasing();
+#     patient_object.assign_to_parent();
+#     patient_object.convert_to_dataframe();
+#     complete_df.append(patient_object.parent_df);
+
+# print(complete_df);
