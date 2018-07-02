42d9524e05620bbffbeaf87c91a870cbb43ce942
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-02 15:26:41 -0400

    fixed pandas data

--- PhasedData.py
+++ PhasedData.py
@@ -212,8 +212,8 @@ class PhasedData:
         df = df[['ID', 'Chrom', 'Location', 'Mom Count', 'Dad Count', 'From Mom',
                  'From Dad', 'Troubleshoot', 'Unphased']];
 
-        df.groupby('ID').sum();
-        totals = df.loc[:['ID', 'From Mom', 'From Dad', 'Troubleshoot', 'Unphased']];
+        totals.groupby('ID').sum();
+        totals.loc[:,['ID', 'From Mom', 'From Dad', 'Troubleshoot', 'Unphased']];
 
 
         print(totals);
