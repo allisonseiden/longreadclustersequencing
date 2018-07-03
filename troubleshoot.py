0b3de90e4d04a78c957a94ba239783d7b5c9c515
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-03 08:33:17 -0400

    troubleshooting error

--- troubleshoot.py
+++ troubleshoot.py
@@ -28,11 +28,20 @@ from PhasedData import PhasedData
 # print(patient_4.parent_df);
 
 
-patient_5 = PhasedData('1-04389');
-patient_5.create_vcf_dictionary();
-patient_5.create_dnvs_dictionary();
-patient_5.fill_bounds_dictionary();
-patient_5.find_variants_for_phasing();
-patient_5.assign_to_parent();
-patient_5.convert_to_dataframe();
-print(patient_5.parent_df);
+# patient_5 = PhasedData('1-04389');
+# patient_5.create_vcf_dictionary();
+# patient_5.create_dnvs_dictionary();
+# patient_5.fill_bounds_dictionary();
+# patient_5.find_variants_for_phasing();
+# patient_5.assign_to_parent();
+# patient_5.convert_to_dataframe();
+# print(patient_5.parent_df);
+
+patient_6 = PhasedData('1-04460');
+patient_6.create_vcf_dictionary();
+patient_6.create_dnvs_dictionary();
+patient_6.fill_bounds_dictionary();
+patient_6.find_variants_for_phasing();
+patient_6.assign_to_parent();
+patient_6.convert_to_dataframe();
+print(patient_6.parent_df);
