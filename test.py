63b022d37008a3add097e5383ca9d452234a5580
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-03 08:40:08 -0400

    troubleshooting error

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from PhasedData import PhasedData
 
 
-patient = PhasedData("1-01019");
+patient = PhasedData("1-00801");
 patient.create_vcf_dictionary();
 patient.create_dnvs_dictionary();
 patient.fill_bounds_dictionary();
 patient.find_variants_for_phasing();
 patient.assign_to_parent();
 patient.convert_to_dataframe();
-print(patient.parent_df);
+print(patient.phased_to_parent);
