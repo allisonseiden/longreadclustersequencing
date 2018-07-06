edad60e66e9dcfc23d9b50a49155c57412e90c32
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-06 14:20:18 -0400

    fixing bed file error

--- phasing/PhasedData.py
+++ phasing/PhasedData.py
@@ -371,7 +371,6 @@ class PhasedData:
     def write_to_bed(self):
         filename = "/hpc/users/seidea02/www/PacbioProject/WhatshapVCFs/" + self.id + "/" + self.id + "_no_indels_dnvs.bed";
         bed_file = open(filename, "w");
-        bed_file.write('Chrom\tStart\tEnd\tRef\tVar\tID\n');
         trouble_length = self.trouble.shape[0];
         for i in range(0, trouble_length):
             line = str(self.trouble['Chrom'][i]) + '\t.\t' + str(self.trouble['Location'][i]) + '\t.\t.\t' + self.id + '\n';
