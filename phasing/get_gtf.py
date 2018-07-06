08a8c7c7a9c026c14f8101607afba67eb6e1195b
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-06 12:18:40 -0400

    script to generate gtf files

---
+++ phasing/get_gtf.py
@@ -0,0 +1,16 @@
+import subprocess as sp
+import multiprocessing as mp
+
+patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05673", "1-05846"];
+
+def get_gtf(ID):
+    for i in range(1,23):
+        command = "whatshap stats --gtf=" + ID + "_chr" + str(i) + "phased.gtf " + ID;
+        command += "_chr" + str(i) + "_phased.vcf";
+        sp.call(command, shell=True);
+        print("Created gtf for " + ID + " chromosome " + str(i));
+
+
+if __name__ == '__main__':
+    pool = mp.Pool(processes=5);
+    pool.map(get_gtf, patientID)
