9269c042f3b72f6e29db8c4116dc5cc644d45b9a
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-06 13:07:08 -0400

    script to generate gtf files

--- phasing/get_gtf.py
+++ phasing/get_gtf.py
@@ -6,7 +6,7 @@ patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "
 def get_gtf(ID):
     for i in range(1,23):
         command = "whatshap stats --gtf=" + ID + "_chr" + str(i) + "phased.gtf " + ID;
-        command += "/" + ID + "_chr" + str(i) + "_phased.vcf";
+        command += "_no_indels/" + ID + "_chr" + str(i) + "_phased.vcf";
         sp.call(command, shell=True);
         print("Created gtf for " + ID + " chromosome " + str(i));
 
