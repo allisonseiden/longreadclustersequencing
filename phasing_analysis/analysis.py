a99933d8392f2b3d64eddc3375a8346d93e6278b
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-11 09:18:50 -0400

    working out dataframe details

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -4,10 +4,12 @@ import numpy as np
 # last patient ID not in list because fixing bam
 patientIDs = ['1-00801', '1-01019', '1-03897', '1-04190', '1-04389', '1-04460',
                 '1-04537', '1-05443', '1-05673'];
-bed_dictionary = {};
-dnv_df = pd.DataFrame();
+bed_list = [];
+
 
 for ID in patientIDs:
-    bed_dictionary[ID] = pd.read_table('/hpc/users/seidea02/www/PacbioProject/DNV_calls/BED/' + ID + '.hg38.dnv.bed',
-                                sep='\t', names = ['Chrom', 'Start', 'Loc', 'Ref', 'Alt', 'ID']);
-    print(bed_dictionary[ID]);
+    bed_list.append(pd.read_table('/hpc/users/seidea02/www/PacbioProject/DNV_calls/BED/' + ID + '.hg38.dnv.bed',
+                                sep='\t', names = ['Chrom', 'Start', 'Loc', 'Ref', 'Alt', 'ID']));
+
+dnv_df = pd.concat(bed_list, ignore_index=True);
+print(dnv_df);
