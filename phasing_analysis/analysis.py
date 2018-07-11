674baa7378868920d0b8474bc0cb643b3dd5920d
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-11 14:15:17 -0400

    working on closest dnv column

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -62,7 +62,7 @@ def find_difference(group):
                 distance_list.append(d_1);
             else:
                 distance_list.append(d_2);
-    print(distance_list);
+    print(group['Ref']);
     # d_series = pd.Series(data=distance_list);
     # group['Closest DNV Distance'] = d_series;
     # group['Closest DNV Distance'] = group['Closest DNV Distance'].astype(int);
