c413aca923e500047cba21e53743b5753c5955e7
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-11 11:21:52 -0400

    working on closest dnv column

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -10,36 +10,37 @@ df_list = [];
 
 for ID in patientIDs:
     bed_list.append(pd.read_table('/hpc/users/seidea02/www/PacbioProject/DNV_calls/BED/' + ID + '.hg38.dnv.bed',
-                                sep='\t', names = ['Chrom', 'Start', 'Location', 'Ref', 'Alt', 'ID']));
+                                sep='\t', names = ['Chrom', 'Start', 'End', 'Ref', 'Alt', 'ID']));
     df_list.append(pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasing_analysis/' + ID + '_dataframe.txt',
                                 sep='\t'));
 
 dnv_df = pd.concat(bed_list, ignore_index=True);
 parent_df = pd.concat(df_list, ignore_index=True);
 
-dnv_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
-parent_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
+dnv_df.set_index(['ID', 'Chrom'], inplace=True);
+parent_df.set_index(['ID', 'Chrom'], inplace=True);
 
 analysis_df = dnv_df.join(parent_df, how='left');
 
-
-ti_series = (((analysis_df['Ref'] == 'A') & (analysis_df['Alt'] == 'G')) |
-             ((analysis_df['Ref'] == 'G') & (analysis_df['Alt'] == 'A')) |
-             ((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'T')) |
-             ((analysis_df['Ref'] == 'T') & (analysis_df['Alt'] == 'C')));
-tv_series = (((analysis_df['Ref'] == 'A') & ((analysis_df['Alt'] == 'T') | (analysis_df['Alt'] == 'C'))) |
-             ((analysis_df['Ref'] == 'G') & ((analysis_df['Alt'] == 'T') | (analysis_df['Alt'] == 'C'))) |
-             ((analysis_df['Ref'] == 'T') & ((analysis_df['Alt'] == 'A') | (analysis_df['Alt'] == 'G'))) |
-             ((analysis_df['Ref'] == 'C') & ((analysis_df['Alt'] == 'A') | (analysis_df['Alt'] == 'G'))));
-
-
-analysis_df['Ti'] = ti_series;
-analysis_df['Tv'] = tv_series;
-analysis_df['Ti'] = analysis_df['Ti'].astype(int);
-analysis_df['Tv'] = analysis_df['Tv'].astype(int);
-
-analysis_df = analysis_df[['Ref', 'Alt', 'Ti', 'Tv', 'From Mom', 'From Dad', 'Unphased']];
-
-grouped = analysis_df.groupby(['ID', 'Chrom']);
-for name, group in grouped:
-    print(group['Ref']);
+print(analysis_df);
+#
+# ti_series = (((analysis_df['Ref'] == 'A') & (analysis_df['Alt'] == 'G')) |
+#              ((analysis_df['Ref'] == 'G') & (analysis_df['Alt'] == 'A')) |
+#              ((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'T')) |
+#              ((analysis_df['Ref'] == 'T') & (analysis_df['Alt'] == 'C')));
+# tv_series = (((analysis_df['Ref'] == 'A') & ((analysis_df['Alt'] == 'T') | (analysis_df['Alt'] == 'C'))) |
+#              ((analysis_df['Ref'] == 'G') & ((analysis_df['Alt'] == 'T') | (analysis_df['Alt'] == 'C'))) |
+#              ((analysis_df['Ref'] == 'T') & ((analysis_df['Alt'] == 'A') | (analysis_df['Alt'] == 'G'))) |
+#              ((analysis_df['Ref'] == 'C') & ((analysis_df['Alt'] == 'A') | (analysis_df['Alt'] == 'G'))));
+#
+#
+# analysis_df['Ti'] = ti_series;
+# analysis_df['Tv'] = tv_series;
+# analysis_df['Ti'] = analysis_df['Ti'].astype(int);
+# analysis_df['Tv'] = analysis_df['Tv'].astype(int);
+#
+# analysis_df = analysis_df[['Ref', 'Alt', 'Ti', 'Tv', 'From Mom', 'From Dad', 'Unphased']];
+#
+# grouped = analysis_df.groupby(['ID', 'Chrom']);
+# for name, group in grouped:
+#     print(group['Ref']);
