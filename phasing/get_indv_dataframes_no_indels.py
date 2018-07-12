d758d069ba7e42aeeb6d38e5963f7f5dacd0a2f2
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-12 08:46:48 -0400

    getting dataframe for patient 10 and adding to analysis.py

--- phasing/get_indv_dataframes_no_indels.py
+++ phasing/get_indv_dataframes_no_indels.py
@@ -70,33 +70,33 @@ from PhasedData import PhasedData
 # patient_7.write_to_bed();
 # print(patient_7.parent_df);
 
-patient_8 = PhasedData('1-05443');
-patient_8.create_vcf_no_indels();
-patient_8.create_dnvs_dictionary();
-patient_8.fill_bounds_dictionary();
-patient_8.find_variants_for_phasing(7);
-patient_8.assign_to_parent();
-patient_8.convert_to_dataframe();
+# patient_8 = PhasedData('1-05443');
+# patient_8.create_vcf_no_indels();
+# patient_8.create_dnvs_dictionary();
+# patient_8.fill_bounds_dictionary();
+# patient_8.find_variants_for_phasing(7);
+# patient_8.assign_to_parent();
+# patient_8.convert_to_dataframe();
 # patient_8.write_to_bed();
 # print(patient_8.parent_df);
 #
 #
-patient_9 = PhasedData('1-05673');
-patient_9.create_vcf_no_indels();
-patient_9.create_dnvs_dictionary();
-patient_9.fill_bounds_dictionary();
-patient_9.find_variants_for_phasing(7);
-patient_9.assign_to_parent();
-patient_9.convert_to_dataframe();
+# patient_9 = PhasedData('1-05673');
+# patient_9.create_vcf_no_indels();
+# patient_9.create_dnvs_dictionary();
+# patient_9.fill_bounds_dictionary();
+# patient_9.find_variants_for_phasing(7);
+# patient_9.assign_to_parent();
+# patient_9.convert_to_dataframe();
 # patient_9.write_to_bed();
 # print(patient_9.parent_df);
 
-# patient_10 = PhasedData('1-05846');
-# patient_10.create_vcf_no_indels();
-# patient_10.create_dnvs_dictionary();
-# patient_10.fill_bounds_dictionary();
-# patient_10.find_variants_for_phasing(7);
-# patient_10.assign_to_parent();
-# patient_10.convert_to_dataframe();
+patient_10 = PhasedData('1-05846');
+patient_10.create_vcf_no_indels();
+patient_10.create_dnvs_dictionary();
+patient_10.fill_bounds_dictionary();
+patient_10.find_variants_for_phasing(7);
+patient_10.assign_to_parent();
+patient_10.convert_to_dataframe();
 # # patient_10.write_to_bed();
 # print(patient_10.parent_df);
--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # last patient ID not in list because fixing bam
 patientIDs = ['1-00801', '1-01019', '1-03897', '1-04190', '1-04389', '1-04460',
-                '1-04537', '1-05443', '1-05673'];
+                '1-04537', '1-05443', '1-05673', '1-05846'];
 bed_list = [];
 df_list = [];
 
