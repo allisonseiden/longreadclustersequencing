0b418a57d269454916f2ebf724517d3c4c4ef84e
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-12 09:16:19 -0400

    phasing to parent for illumina patient 1

--- phasing/get_indv_dataframes.py
+++ phasing/get_indv_dataframes.py
@@ -7,21 +7,21 @@ patient_1.fill_bounds_dictionary();
 patient_1.find_variants_for_phasing(7);
 patient_1.assign_to_parent();
 patient_1.convert_to_dataframe();
-print(patient_1.trouble);
-print(patient_1.unphased);
-print(patient_1.parent_df);
+# print(patient_1.trouble);
+# print(patient_1.unphased);
+# print(patient_1.parent_df);
 
 
-patient_2 = PhasedData('1-01019');
-patient_2.create_vcf_dictionary();
-patient_2.create_dnvs_dictionary();
-patient_2.fill_bounds_dictionary();
-patient_2.find_variants_for_phasing(7);
-patient_2.assign_to_parent();
-patient_2.convert_to_dataframe();
-print(patient_2.trouble);
-print(patient_2.unphased);
-print(patient_2.parent_df);
+# patient_2 = PhasedData('1-01019');
+# patient_2.create_vcf_dictionary();
+# patient_2.create_dnvs_dictionary();
+# patient_2.fill_bounds_dictionary();
+# patient_2.find_variants_for_phasing(7);
+# patient_2.assign_to_parent();
+# patient_2.convert_to_dataframe();
+# print(patient_2.trouble);
+# print(patient_2.unphased);
+# print(patient_2.parent_df);
 
 # patient_3 = PhasedData('1-03897');
 # patient_3.create_vcf_dictionary();
