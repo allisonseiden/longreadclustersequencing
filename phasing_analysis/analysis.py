1f06a3a6d89498b3fec43ee1f78c19f71c70f993
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-13 13:48:24 -0400

    working on adding CpG island de novos to dataframe

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -36,11 +36,11 @@ for ID in patientIDs:
                         names=['Chrom', 'Start', 'Location', 'Ref', 'Alt', 'ID']));
 
 dnv_bed_df = pd.concat(dnv_bed_list, ignore_index=True);
-dnv_bed_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 CpG_i_length = dnv_bed_df.shape[0];
 all_ones = [1] * CpG_i_length;
 CpG_island = pd.Series(all_ones);
 dnv_bed_df['CpG_island'] = CpG_island;
+dnv_bed_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 print(dnv_bed_df);
 
