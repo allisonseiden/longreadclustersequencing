1a2c2f512d7b519d5465e47818953e91ad21a654
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-13 14:23:27 -0400

    changing decimal to integer

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -39,17 +39,14 @@ dnv_bed_df = pd.concat(dnv_bed_list, ignore_index=True);
 CpG_i_length = dnv_bed_df.shape[0];
 all_ones = np.ones(CpG_i_length, dtype=int);
 dnv_bed_df['CpG_Island'] = all_ones;
+dnv_bed_df = dnv_bed_df[['ID', 'Chrom', 'Location', 'CpG_Island']];
 dnv_bed_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
-dnv_bed_df = dnv_bed_df[['CpG_Island']];
-dnv_bed_df['CpG_Island'].astype('int64');
 
 temp_one_df = dnv_df.join(pb_parent_df, how='left');
 temp_two_df = temp_one_df.join(il_parent_df, how='left');
 analysis_df = temp_two_df.join(dnv_bed_df, how='left');
-
-
 analysis_df.fillna(value=0, inplace=True);
-analysis_df['CpG_Island'].astype('int64');
+
 
 
 ti_series = (((analysis_df['Ref'] == 'A') & (analysis_df['Alt'] == 'G')) |
@@ -98,6 +95,6 @@ def find_difference(group):
 grouped = analysis_df.groupby(['ID', 'Chrom']);
 analysis_df = grouped.apply(find_difference);
 
-
+analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 print(analysis_df);
