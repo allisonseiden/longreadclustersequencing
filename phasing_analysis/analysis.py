dda9b7ff360c433ca63d7b26e8c66f38cd2d8c75
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-13 13:54:36 -0400

    working on adding CpG island de novos to dataframe

--- phasing_analysis/analysis.py
+++ phasing_analysis/analysis.py
@@ -42,8 +42,9 @@ CpG_island = pd.Series(all_ones);
 dnv_bed_df['CpG_island'] = CpG_island;
 dnv_bed_df = dnv_bed_df[['ID', 'Chrom', 'Location', 'CpG_island']];
 dnv_bed_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
-print(dnv_bed_df);
-#
+
+analysis_df = dnv_df.join(dnv_bed_df, how='left');
+print(analysis_df);
 # temp_one_df = dnv_df.join(pb_parent_df, how='left');
 # temp_two_df = temp_one_df.join(il_parent_df, how='left');
 # analysis_df = temp_two_df.join(dnv_bed_df, how='left');
