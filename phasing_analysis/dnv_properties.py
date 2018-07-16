11a24c947305e2092956d77dcb7ed6e0f962b599
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-16 15:51:46 -0400

    C->A mutational class phased versus unphased

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -6,11 +6,11 @@ analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasi
 analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 C_A_pb_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
-              ((analysis_df['PB_Mom'] == 1) | (analysis_df['PB_Dad'] == 1)));
+              ((analysis_df['PB_Mom']) | (analysis_df['PB_Dad'])));
 C_A_pb_phased = C_A_pb_phased.astype(int);
 
 C_A_il_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
-              ((analysis_df['IL_Mom'] == 1) | (analysis_df['IL_Dad'] == 1)));
+              ((analysis_df['IL_Mom']) | (analysis_df['IL_Dad'])));
 C_A_il_phased = C_A_il_phased.astype(int);
 
 C_A_phased_unphased = pd.concat([C_A_pb_phased, C_A_il_phased], axis=1);
