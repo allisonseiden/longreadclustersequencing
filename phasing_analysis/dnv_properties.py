bd3082cbad560f2763df7927b5964d493c995e91
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-16 15:31:11 -0400

    starting de novo analysis using dataframe created with analysis.py

---
+++ phasing_analysis/dnv_properties.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
+                            sep='\t');
+print(analysis_df);
