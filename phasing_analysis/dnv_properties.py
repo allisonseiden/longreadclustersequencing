ad35854602fa5ecf5bc74f1538faf2406409d337
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-16 15:49:02 -0400

    C->A mutational class phased versus unphased

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -7,9 +7,11 @@ analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 C_A_pb_phased = ((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A') &
               ((analysis_df['PB_Mom'] == 1) | (analysis_df['PB_Dad'] == 1)));
+C_A_pb_phased = C_A_pb_phased.astype(int);
 
 C_A_il_phased = ((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A') &
               ((analysis_df['IL_Mom'] == 1) | (analysis_df['IL_Dad'] == 1)));
+C_A_il_phased = C_A_il_phased.astype(int);
 
 C_A_phased_unphased = pd.concat([C_A_pb_phased, C_A_il_phased], axis=1);
 
