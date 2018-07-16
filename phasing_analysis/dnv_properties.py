fe02de0a91d7cac63c16fb677fc0b876ba77af49
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-16 15:58:15 -0400

    C->A mutational class phased versus unphased

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -3,8 +3,7 @@ import numpy as np
 
 analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
                             sep='\t');
-analysis_df.set_index(['ID', 'Chrom', 'Location', 'PB_Mom', 'PB_Dad', 'PB_Unphased',
-                        'IL_Mom', 'IL_Dad', 'IL_Unphased'], inplace=True);
+analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 # C_A_pb_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
 #               ((analysis_df['PB_Mom']) | (analysis_df['PB_Dad'])));
@@ -13,8 +12,10 @@ analysis_df.set_index(['ID', 'Chrom', 'Location', 'PB_Mom', 'PB_Dad', 'PB_Unphas
 # C_A_il_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
 #               ((analysis_df['IL_Mom']) | (analysis_df['IL_Dad'])));
 # C_A_il_phased = C_A_il_phased.astype(int);
-C_A = ((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A'));
+group_by_ref_alt = analysis_df.groupby(['Ref', 'Alt']);
 
 # C_A_phased_unphased = pd.concat([C_A_pb_phased, C_A_il_phased], axis=1);
 
-print(C_A);
+for name, group in group_by_ref_alt:
+    print(name);
+    print(group);
