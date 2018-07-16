932bc2b807432a30055b7f5e3c28b1abd088602f
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-16 15:38:25 -0400

    figuring out which columns are best to group by

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -3,9 +3,4 @@ import numpy as np
 
 analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
                             sep='\t');
-phased_unphased_pacbio = analysis_df.groupby(['ID', 'Chrom', 'Location',
-                                                        'PB_Mom', 'PB_Dad',
-                                                        'PB_Unphased']);
-for name, group in phased_unphased_pacbio:
-    print(name);
-    print(group);               
+analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
