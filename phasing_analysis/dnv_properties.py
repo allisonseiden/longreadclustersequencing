6c7bb7e47f76f7480ad61484adf5abb669459611
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-17 07:49:13 -0400

    working on C->A mutation phased/unphased rates

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -6,8 +6,11 @@ analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasi
 analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
 group_by_ref_alt = analysis_df.groupby(['ID', 'Ref', 'Alt']);
-
+C_A_df_list = [];
 
 for name, group in group_by_ref_alt:
     if name[1] == 'C' and name[2] == 'A':
-        print(type(group));
+        C_A_df_list.append(group);
+
+C_A_df = pd.concat(C_A_df_list, ignore_index=True);
+print(C_A_df);
