a6ba2a4914f5b4ee063ce31ea9e0c1be9c8dd911
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-17 07:45:51 -0400

    working on C->A mutation phased/unphased rates

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -5,16 +5,8 @@ analysis_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/phasi
                             sep='\t');
 analysis_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
-# C_A_pb_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
-#               ((analysis_df['PB_Mom']) | (analysis_df['PB_Dad'])));
-# C_A_pb_phased = C_A_pb_phased.astype(int);
-#
-# C_A_il_phased = (((analysis_df['Ref'] == 'C') & (analysis_df['Alt'] == 'A')) &
-#               ((analysis_df['IL_Mom']) | (analysis_df['IL_Dad'])));
-# C_A_il_phased = C_A_il_phased.astype(int);
-group_by_ref_alt = analysis_df.groupby(['Ref', 'Alt']);
+group_by_ref_alt = analysis_df.groupby(['ID', 'Ref', 'Alt']);
 
-# C_A_phased_unphased = pd.concat([C_A_pb_phased, C_A_il_phased], axis=1);
 
 for name, group in group_by_ref_alt:
     if name[0] == 'C' and name[1] == 'A':
