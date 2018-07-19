8c69b8ba18094c1403000747af65cffa5e569875
Allison Seiden <Seiden@allisons-mbp.wireless.mountsinai.org>
2018-07-19 10:58:25 -0400

    pushing program that created graphs, two completed graphs, and dataframe manipulation program

---
+++ phasing_analysis/create_graphs.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import wes
+
+
+sb.set_context("paper");
+sb.set_style("ticks", {"font.family" : ['calibri']})
+dnv_data = pd.read_csv('dnv_parent_percentages.txt', sep='\t', engine='python');
+dnv_data.set_index(['Mutational_Class'], inplace=True);
+mutation_list = ['C_A', 'C_T', 'C_G', 'T_A', 'T_C', 'T_G', 'CpG_TpG'];
+length = len(mutation_list);
+
+f, axarr = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 3));
+y_pos = [0.2625, 0.7375];
+plt.yticks([]);
+
+for i in range(length):
+    curr_df = dnv_data.loc[mutation_list[i]];
+    curr_df.reset_index(inplace=True);
+    fraction_list = curr_df['Fraction'].tolist();
+    if i < 4:
+        axarr[0, i].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[0, i].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 4:
+        axarr[1, 0].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[1, 0].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 5:
+        axarr[1, 1].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[1, 1].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    else:
+        dad = axarr[1, 2].barh(y_pos[0], fraction_list[0], color='#352846', left=0.005, height=0.425, label='Father');
+        mom = axarr[1, 2].barh(y_pos[1], fraction_list[1], color='#E2AE38', left=0.005, height=0.425, label='Mother');
+        axarr[1, 2].set_title('CpG  >  TpG');
+f.text(0.5, 0.06, 'Fraction', ha='center', va='center');
+f.text(0.02, 0.5, 'Mutational Class', ha='center', va='center', rotation='vertical');
+f.tight_layout(pad=2.5, w_pad=1.0, h_pad=1.0);
+f.legend(handles=[dad, mom],loc=(0.776, 0.02), ncol=2, borderaxespad=0.);
+
+plt.savefig("mom_dad_comparison_bar_chart.png");
+
+patientIDs = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
+mutation_list = ['C_A', 'C_T', 'C_G', 'T_A', 'T_C', 'T_G', 'CpG_TpG'];
+length = len(mutation_list);
+
+parental_age = pd.read_csv('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/data/parental_age_at_conception.txt', sep='\t', engine='python');
+parental_age.set_index(['ID'], inplace=True);
+parental_fractions = pd.read_csv('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/phasing_analysis/dnv_parent_percentages_by_ID.txt', sep='\t', engine='python');
+parental_fractions.set_index(['ID'], inplace=True);
+dnv_data = parental_age.join(parental_fractions, how='left');
+
+paternal_data = dnv_data[dnv_data.Parent == 'Father'];
+maternal_data = dnv_data[dnv_data.Parent == 'Mother'];
+
+
+f, axarr = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 5));
+
+for i in range(length):
+    paternal_mut_df = paternal_data[paternal_data.Mutational_Class == mutation_list[i]];
+    maternal_mut_df = maternal_data[maternal_data.Mutational_Class == mutation_list[i]];
+    age_list_dad = paternal_mut_df['Paternal_age_at_conception'].tolist();
+    age_list_mom = maternal_mut_df['Maternal_age_at_conception'].tolist();
+    fraction_list_dad = paternal_mut_df['Fraction'].tolist();
+    fraction_list_mom = maternal_mut_df['Fraction'].tolist();
+    if i < 4:
+        dad = axarr[0, i].scatter(age_list_dad, fraction_list_dad, c="#352846", label='Father');
+        mom = axarr[0, i].scatter(age_list_mom, fraction_list_mom, c="#E2AE38", label='Mother');
+        axarr[0, i].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 4:
+        dad = axarr[1, 0].scatter(age_list_dad, fraction_list_dad, c="#352846", label='Father');
+        mom = axarr[1, 0].scatter(age_list_mom, fraction_list_mom, c="#E2AE38", label='Mother');
+        axarr[1, 0].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 5:
+        dad = axarr[1, 1].scatter(age_list_dad, fraction_list_dad, c="#352846", label='Father');
+        mom = axarr[1, 1].scatter(age_list_mom, fraction_list_mom, c="#E2AE38", label='Mother');
+        axarr[1, 1].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    else:
+        dad = axarr[1, 2].scatter(age_list_dad, fraction_list_dad, c="#352846", label='Father');
+        mom = axarr[1, 2].scatter(age_list_mom, fraction_list_mom, c="#E2AE38", label='Mother');
+        axarr[1, 2].set_title('CpG  >  TpG');
+
+f.text(0.02, 0.5, 'Fraction', ha='center', va='center', rotation='vertical');
+f.text(0.5, 0.035, 'Age of Parent at Conception (yr)', ha='center');
+f.tight_layout(pad=2.5, w_pad=1.0, h_pad=1.0);
+f.legend(handles=[dad, mom],loc=(0.776, 0.02), ncol=2, borderaxespad=0.);
+plt.savefig("mom_dad_age_scatter.png");
--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -3,12 +3,23 @@ import numpy as np
 
 analysis_df = pd.read_table('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
                             sep='\t');
-dnv_data_dict = {};
-dnv_data_dict['Total'] = analysis_df.sum(numeric_only=True);
-dnv_data_dict['Total'].drop(['Location', 'Ti', 'Tv', 'Closest DNV Distance'], inplace=True);
-dnv_data_dict['Total'] = dnv_data_dict['Total'].append(pd.Series(['All'], index=['Mutational_Class']));
 
-group_by_ref_alt = analysis_df.groupby(['Ref', 'Alt']);
+pacbio_df = analysis_df.loc[:,['ID', 'Chrom', 'Location', 'Ref', 'Alt', 'PB_Mom', 'PB_Dad', 'PB_Unphased', 'CpG']];
+length = analysis_df.shape[0];
+for i in range(length):
+    if pacbio_df['PB_Mom'][i] == 1:
+        pacbio_df.loc[i, 'Parent'] = 'Mother';
+    elif pacbio_df ['PB_Dad'][i] == 1:
+        pacbio_df.loc[i, 'Parent'] = 'Father';
+    else:
+        pacbio_df.loc[i, 'Parent'] = 'None';
+
+dnv_nums_dict = {};
+dnv_nums_dict['Total'] = pacbio_df.sum(numeric_only=True);
+dnv_nums_dict['Total'].drop(['Location'], inplace=True);
+dnv_nums_dict['Total'] = dnv_nums_dict['Total'].append(pd.Series(['All'], index=['Mutational_Class']));
+
+group_by_ref_alt = pacbio_df.groupby(['Ref', 'Alt']);
 mutation_groups = {'C_A' : [], 'C_T' : [], 'C_G' : [], 'T_A' : [], 'T_C' : [], 'T_G' : [], 'CpG_TpG' : []};
 # mutation_groups = {'C_A' : [], 'G_T' : [], 'C_T' : [], 'G_A' : [], 'C_G' : [], 'G_C' : [],
 #                     'T_A' : [], 'A_T' : [], 'T_C' : [], 'A_G' : [], 'T_G' : [], 'A_C' : []};
@@ -28,65 +39,38 @@ for name, group in group_by_ref_alt:
         mutation_groups['T_C'].append(group);
     if (name[0] == 'T' and name[1] == 'G') or (name[0] == 'A' and name[1] == 'C'):
         mutation_groups['T_G'].append(group);
-    # if (name[0] == 'C' and name[1] == 'A'):
-    #     mutation_groups['C_A'].append(group);
-    # if (name[0] == 'G' and name[1] == 'T'):
-    #     mutation_groups['G_T'].append(group);
-    # if (name[0] == 'C' and name[1] == 'T'):
-    #     mutation_groups['C_T'].append(group);
-    # if (name[0] == 'G' and name[1] == 'A'):
-    #     mutation_groups['G_A'].append(group);
-    # if (name[0] == 'C' and name[1] == 'G'):
-    #     mutation_groups['C_G'].append(group);
-    # if (name[0] == 'G' and name[1] == 'C'):
-    #     mutation_groups['G_C'].append(group);
-    # if (name[0] == 'T' and name[1] == 'A'):
-    #     mutation_groups['T_A'].append(group);
-    # if (name[0] == 'A' and name[1] == 'T'):
-    #     mutation_groups['A_T'].append(group);
-    # if (name[0] == 'T' and name[1] == 'C'):
-    #     mutation_groups['T_C'].append(group);
-    # if (name[0] == 'A' and name[1] == 'G'):
-    #     mutation_groups['A_G'].append(group);
-    # if (name[0] == 'T' and name[1] == 'G'):
-    #     mutation_groups['T_G'].append(group);
-    # if (name[0] == 'A' and name[1] == 'C'):
-    #     mutation_groups['A_C'].append(group);
 
-pb_total_mom = float(dnv_data_dict['Total']['PB_Mom']);
-pb_total_dad = float(dnv_data_dict['Total']['PB_Dad']);
-pb_total_phased = float(dnv_data_dict['Total']['PB_Mom'] + dnv_data_dict['Total']['PB_Dad']);
-pb_total_unphased = float(dnv_data_dict['Total']['PB_Unphased']);
-il_total_mom = float(dnv_data_dict['Total']['IL_Mom']);
-il_total_dad = float(dnv_data_dict['Total']['IL_Dad']);
-il_total_phased = float(dnv_data_dict['Total']['IL_Mom'] + dnv_data_dict['Total']['IL_Dad']);
-il_total_unphased = float(dnv_data_dict['Total']['IL_Unphased']);
 
+
+pb_total_mom = float(dnv_nums_dict['Total']['PB_Mom']);
+pb_total_dad = float(dnv_nums_dict['Total']['PB_Dad']);
+pb_total_unphased = float(dnv_nums_dict['Total']['PB_Unphased']);
+# il_total_mom = float(dnv_data_dict['Total']['IL_Mom']);
+# il_total_dad = float(dnv_data_dict['Total']['IL_Dad']);
+# il_total_unphased = float(dnv_data_dict['Total']['IL_Unphased']);
+dnv_percent_dict = {};
 for key in mutation_groups:
     full_data_dfs[key] = pd.concat(mutation_groups[key], ignore_index=True);
-    full_data_dfs[key].set_index(['ID', 'Chrom', 'Location'], inplace=True);
-    dnv_data_dict[key] = full_data_dfs[key].sum(numeric_only=True);
-    dnv_data_dict[key].drop(['Ti', 'Tv', 'Closest DNV Distance'], inplace=True);
-    pb_phased = (dnv_data_dict[key]['PB_Mom'] + dnv_data_dict[key]['PB_Dad'])/pb_total_phased;
-    pb_mom = dnv_data_dict[key]['PB_Mom']/pb_total_mom;
-    pb_dad = dnv_data_dict[key]['PB_Dad']/pb_total_dad;
-    pb_unphased = (dnv_data_dict[key]['PB_Unphased'])/pb_total_unphased;
-    il_phased = (dnv_data_dict[key]['IL_Mom'] + dnv_data_dict[key]['IL_Dad'])/il_total_phased;
-    il_mom = dnv_data_dict[key]['IL_Mom']/il_total_mom;
-    il_dad = dnv_data_dict[key]['IL_Dad']/il_total_dad;
-    il_unphased = (dnv_data_dict[key]['IL_Unphased'])/il_total_unphased;
-    dnv_data_dict[key] = dnv_data_dict[key].append(pd.Series([pb_mom, pb_dad, pb_phased, pb_unphased, il_mom, il_dad, il_phased, il_unphased], index=['PB_Percent_Mom', 'PB_Percent_Dad', 'PB_Percent_Phased', 'PB_Percent_Unphased', 'IL_Percent_Mom', 'IL_Percent_Dad', 'IL_Percent_Phased', 'IL_Percent_Unphased']));
-    dnv_data_dict[key] = dnv_data_dict[key].append(pd.Series([str(key)], index=['Mutational_Class']));
+    dnv_nums_dict[key] = full_data_dfs[key].groupby(['ID', 'Parent']).sum(numeric_only=True);
+    pb_mom_series = dnv_nums_dict[key]['PB_Mom']/pb_total_mom;
+    pb_dad_series = dnv_nums_dict[key]['PB_Dad']/pb_total_dad;
+    temp = pd.concat([pb_mom_series, pb_dad_series], axis=1);
+    temp.reset_index(inplace=True);
+    mutational_class_series = pd.Series([key] * 30);
+    dnv_percent_dict[key] = pd.concat([temp, mutational_class_series], axis=1);
+    dnv_percent_dict[key].rename(columns={0 : 'Mutational_Class'}, inplace=True);
+    dnv_percent_dict[key].fillna(value=0, inplace=True);
+    dnv_percent_dict[key]['Fraction'] = dnv_percent_dict[key].iloc[:, 2:4].sum(axis=1);
+    dnv_percent_dict[key] = dnv_percent_dict[key][['ID', 'Mutational_Class', 'Parent', 'Fraction']];
+    # dnv_percent_dict[key].drop([2], inplace=True);
+    dnv_percent_dict[key] = dnv_percent_dict[key][dnv_percent_dict[key].Fraction != 0]
 
-dnv_data = pd.DataFrame(dnv_data_dict).transpose();
-dnv_data.reset_index(inplace=True);
-dnv_data.set_index(['Mutational_Class'], inplace=True);
-dnv_data.drop(['index'], axis=1, inplace=True);
-
-dnv_data = dnv_data[['PB_Mom', 'PB_Percent_Mom', 'PB_Dad', 'PB_Percent_Dad', 'PB_Percent_Phased',
-                    'PB_Unphased', 'PB_Percent_Unphased', 'IL_Mom', 'IL_Percent_Mom',
-                    'IL_Dad', 'IL_Percent_Dad', 'IL_Percent_Phased', 'IL_Unphased',
-                    'IL_Percent_Unphased', 'CpG', 'CpG_Island']];
+    # dnv_percent_dict[key].set_index(['Mutational_Class'], inplace=True);
 
 
-dnv_data.to_csv(path_or_buf='dnv_data.txt', sep='\t');
+dnv_data = pd.concat(dnv_percent_dict);
+dnv_data.set_index(['ID', 'Mutational_Class', 'Parent'], inplace=True);
+
+
+
+dnv_data.to_csv(path_or_buf='dnv_parent_percentages_by_ID.txt', sep='\t');
