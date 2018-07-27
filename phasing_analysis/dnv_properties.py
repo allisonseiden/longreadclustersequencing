310248b6895c422d3381603568eed23fb8b43668
Allison Seiden <allisonseiden@macbook-pro-2.wireless.mountsinai.org>
2018-07-27 15:00:21 -0400

    edited dnv_properties to include indels

--- phasing_analysis/dnv_properties.py
+++ phasing_analysis/dnv_properties.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-analysis_df = pd.read_table('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
+analysis_df = pd.read_table('/Users/allisonseiden/Documents/Summer2018/longreadclustersequencing/phasing_analysis/phasing_analysis_df.txt',
+                            sep='\t');
+indels_df = pd.read_table('/Users/allisonseiden/Documents/Summer2018/longreadclustersequencing/phasing_analysis/indels_df.txt',
                             sep='\t');
 
 pacbio_df = analysis_df.loc[:,['ID', 'Chrom', 'Location', 'PB_Mom', 'PB_Dad', 'PB_Unphased']];
@@ -14,15 +16,21 @@ for i in range(length):
     else:
         pacbio_df.loc[i, 'Parent'] = 'None';
 
-pacbio_df.to_csv(path_or_buf='all_pacbio_data.txt', sep='\t');
+pacbio_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
+indels_df.set_index(['ID', 'Chrom', 'Location'], inplace=True);
 
+df = pacbio_df.join(indels_df, how='left');
+
+df.to_csv(path_or_buf='all_pacbio_data.txt', sep='\t');
+
+df.reset_index(inplace=True);
 
 dnv_nums_dict = {};
-dnv_nums_dict['Total'] = pacbio_df.sum(numeric_only=True);
+dnv_nums_dict['Total'] = df.sum(numeric_only=True);
 dnv_nums_dict['Total'].drop(['Location'], inplace=True);
 dnv_nums_dict['Total'] = dnv_nums_dict['Total'].append(pd.Series(['All'], index=['Mutational_Class']));
 
-group_by_ref_alt = pacbio_df.groupby(['Ref', 'Alt']);
+group_by_ref_alt = df.groupby(['Ref', 'Alt']);
 mutation_groups = {'C_A' : [], 'C_T' : [], 'C_G' : [], 'T_A' : [], 'T_C' : [], 'T_G' : [], 'CpG_TpG' : []};
 # mutation_groups = {'C_A' : [], 'G_T' : [], 'C_T' : [], 'G_A' : [], 'C_G' : [], 'G_C' : [],
 #                     'T_A' : [], 'A_T' : [], 'T_C' : [], 'A_G' : [], 'T_G' : [], 'A_C' : []};
@@ -32,8 +40,8 @@ for name, group in group_by_ref_alt:
     if (name[0] == 'C' and name[1] == 'A') or (name[0] == 'G' and name[1] == 'T'):
         mutation_groups['C_A'].append(group);
     if (name[0] == 'C' and name[1] == 'T') or (name[0] == 'G' and name[1] == 'A'):
-        mutation_groups['CpG_TpG'].append(group.loc[lambda df: df.CpG == 1, :]);
-        mutation_groups['C_T'].append(group.loc[lambda df: df.CpG == 0, :]);
+        mutation_groups['CpG_TpG'].append(group.loc[lambda d: d.CpG == 1, :]);
+        mutation_groups['C_T'].append(group.loc[lambda d: d.CpG == 0, :]);
     if (name[0] == 'C' and name[1] == 'G') or (name[0] == 'G' and name[1] == 'C'):
         mutation_groups['C_G'].append(group);
     if (name[0] == 'T' and name[1] == 'A') or (name[0] == 'A' and name[1] == 'T'):
