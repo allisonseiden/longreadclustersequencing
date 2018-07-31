e1c243a4853e3d477104abc260faf2851ac6ef62
Allison Seiden <allisonseiden@macbook-pro-2.wireless.mountsinai.org>
2018-07-31 10:13:45 -0400

    redoing graphs to include indels

--- phasing_analysis/create_graphs.py
+++ phasing_analysis/create_graphs.py
@@ -7,38 +7,38 @@ import wes
 
 sb.set_context("paper");
 sb.set_style("ticks", {"font.family" : ['calibri']})
-# dnv_data = pd.read_csv('dnv_parent_percentages.txt', sep='\t', engine='python');
-# dnv_data.set_index(['Mutational_Class'], inplace=True);
-# mutation_list = ['C_A', 'C_T', 'C_G', 'T_A', 'T_C', 'T_G', 'CpG_TpG'];
-# length = len(mutation_list);
-#
-# f, axarr = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 3));
-# y_pos = [0.2625, 0.7375];
-# plt.yticks([]);
-#
-# for i in range(length):
-#     curr_df = dnv_data.loc[mutation_list[i]];
-#     curr_df.reset_index(inplace=True);
-#     fraction_list = curr_df['Fraction'].tolist();
-#     if i < 4:
-#         axarr[0, i].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
-#         axarr[0, i].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
-#     elif i == 4:
-#         axarr[1, 0].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
-#         axarr[1, 0].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
-#     elif i == 5:
-#         axarr[1, 1].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
-#         axarr[1, 1].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
-#     else:
-#         dad = axarr[1, 2].barh(y_pos[0], fraction_list[0], color='#352846', left=0.005, height=0.425, label='Father');
-#         mom = axarr[1, 2].barh(y_pos[1], fraction_list[1], color='#E2AE38', left=0.005, height=0.425, label='Mother');
-#         axarr[1, 2].set_title('CpG  >  TpG');
-# f.text(0.5, 0.06, 'Fraction', ha='center', va='center');
-# f.text(0.02, 0.5, 'Mutational Class', ha='center', va='center', rotation='vertical');
-# f.tight_layout(pad=2.5, w_pad=1.0, h_pad=1.0);
-# f.legend(handles=[dad, mom],loc=(0.776, 0.02), ncol=2, borderaxespad=0.);
-#
-# plt.savefig("mom_dad_comparison_bar_chart.png");
+dnv_data = pd.read_csv('dnv_parent_percentages.txt', sep='\t', engine='python');
+dnv_data.set_index(['Mutational_Class'], inplace=True);
+mutation_list = ['C_A', 'C_T', 'C_G', 'T_A', 'T_C', 'T_G', 'CpG_TpG'];
+length = len(mutation_list);
+
+f, axarr = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 3));
+y_pos = [0.2625, 0.7375];
+plt.yticks([]);
+
+for i in range(length):
+    curr_df = dnv_data.loc[mutation_list[i]];
+    curr_df.reset_index(inplace=True);
+    fraction_list = curr_df['Fraction'].tolist();
+    if i < 4:
+        axarr[0, i].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[0, i].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 4:
+        axarr[1, 0].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[1, 0].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    elif i == 5:
+        axarr[1, 1].barh(y_pos, fraction_list, color=["#352846", '#E2AE38'], left=[0.005, 0.005], height=0.425, align='center');
+        axarr[1, 1].set_title(mutation_list[i][0] + '  >  ' + mutation_list[i][2]);
+    else:
+        dad = axarr[1, 2].barh(y_pos[0], fraction_list[0], color='#352846', left=0.005, height=0.425, label='Father');
+        mom = axarr[1, 2].barh(y_pos[1], fraction_list[1], color='#E2AE38', left=0.005, height=0.425, label='Mother');
+        axarr[1, 2].set_title('CpG  >  TpG');
+f.text(0.5, 0.06, 'Fraction', ha='center', va='center');
+f.text(0.02, 0.5, 'Mutational Class', ha='center', va='center', rotation='vertical');
+f.tight_layout(pad=2.5, w_pad=1.0, h_pad=1.0);
+f.legend(handles=[dad, mom],loc=(0.776, 0.02), ncol=2, borderaxespad=0.);
+
+plt.savefig("mom_dad_comparison_bar_chart.png");
 #
 # -------------------- end of mom and dad comparison bar chart
 #
@@ -90,38 +90,38 @@ sb.set_style("ticks", {"font.family" : ['calibri']})
 #
 # --------------------------- end of parental age scatter plot
 
-pacbio_data = pd.read_table('all_pacbio_data.txt', sep='\t', engine='python');
-# f, axarr = plt.subplots(2, 5, sharex=True, sharey=True);
-# chr_list = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr7', 'chr8', 'chr9', 'chr10', 'chr12'];
-# i = 0;
-# for chr in chr_list:
-#     chr_df = pacbio_data[pacbio_data.Chrom == chr];
-#     chr_df_mom = chr_df[chr_df.Parent == 'Mother'];
-#     loc_list = chr_df_mom['Location'].tolist();
-#     if i < 5:
-#         axarr[0, i].hist(loc_list, color="#E2AE38");
-#         axarr[0, i].set_title(chr);
+# pacbio_data = pd.read_table('all_pacbio_data.txt', sep='\t', engine='python');
+# # f, axarr = plt.subplots(2, 5, sharex=True, sharey=True);
+# # chr_list = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr7', 'chr8', 'chr9', 'chr10', 'chr12'];
+# # i = 0;
+# # for chr in chr_list:
+# #     chr_df = pacbio_data[pacbio_data.Chrom == chr];
+# #     chr_df_mom = chr_df[chr_df.Parent == 'Mother'];
+# #     loc_list = chr_df_mom['Location'].tolist();
+# #     if i < 5:
+# #         axarr[0, i].hist(loc_list, color="#E2AE38");
+# #         axarr[0, i].set_title(chr);
+# #     else:
+# #         axarr[1, i-5].hist(loc_list, color="#E2AE38");
+# #         axarr[1, i-5].set_title(chr);
+# #     i += 1;
+# # f.tight_layout();
+#
+# f_2, axarr_2 = plt.subplots(3, 6, sharex=True, sharey=True);
+# for i in range(1, 19):
+#     chrom_name = 'chr' + str(i);
+#     chr_df = pacbio_data[pacbio_data.Chrom == chrom_name];
+#     chr_df_dad = chr_df[chr_df.Parent == 'Father'];
+#     loc_list = chr_df_dad['Location'].tolist();
+#     if i < 7:
+#         axarr_2[0, (i-1)].hist(loc_list, color="#352846");
+#         axarr_2[0, (i-1)].set_title(chrom_name);
+#     elif i < 13:
+#         axarr_2[1, i-7].hist(loc_list, color="#352846");
+#         axarr_2[1, i-7].set_title(chrom_name);
 #     else:
-#         axarr[1, i-5].hist(loc_list, color="#E2AE38");
-#         axarr[1, i-5].set_title(chr);
-#     i += 1;
-# f.tight_layout();
-
-f_2, axarr_2 = plt.subplots(3, 6, sharex=True, sharey=True);
-for i in range(1, 19):
-    chrom_name = 'chr' + str(i);
-    chr_df = pacbio_data[pacbio_data.Chrom == chrom_name];
-    chr_df_dad = chr_df[chr_df.Parent == 'Father'];
-    loc_list = chr_df_dad['Location'].tolist();
-    if i < 7:
-        axarr_2[0, (i-1)].hist(loc_list, color="#352846");
-        axarr_2[0, (i-1)].set_title(chrom_name);
-    elif i < 13:
-        axarr_2[1, i-7].hist(loc_list, color="#352846");
-        axarr_2[1, i-7].set_title(chrom_name);
-    else:
-        axarr_2[2, i-13].hist(loc_list, color="#352846");
-        axarr_2[2, i-13].set_title(chrom_name);
-f_2.tight_layout();
-
-plt.savefig('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/graphs/chromosomal_regions_dad.png');
+#         axarr_2[2, i-13].hist(loc_list, color="#352846");
+#         axarr_2[2, i-13].set_title(chrom_name);
+# f_2.tight_layout();
+#
+# plt.savefig('/Users/Seiden/Documents/Summer2018/longreadclustersequencing/graphs/chromosomal_regions_dad.png');
