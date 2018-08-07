ef55e1fddf95bbd11e5b168ccf68e3e336542fe9
Allison Seiden <ahseiden@gmail.com>
2018-08-07 12:03:26 -0400

    cleaning and organizing

---
+++ phasing/get_ID_dataframes.py
@@ -0,0 +1,22 @@
+""" Run parent assignment algorithm on all patient IDs using phased VCFs from
+    Whatshap software (without indels flag) """
+
+from PhasedData import PhasedData
+import multiprocessing as mp
+
+patientIDs = ['1-00801', '1-01019', '1-03897', '1-04190', '1-04389', '1-04460', '1-04537', '1-05443', '1-05673', '1-05846'];
+
+def get_pacbio_dataframes(ID):
+    patient = PhasedData(ID);
+    patient.pacbio();
+
+def get_illumina_dataframes(ID):
+    patient = PhasedData(ID);
+    patient.illumina();
+
+if __name__ == '__main__':
+  pool = mp.Pool(processes=5);
+  # comment out the one which you're not using at the moment, only call one
+  # of these at a time
+  pool.map(get_pacbio_dataframes, patientIDs);
+  pool.map(get_illumina_dataframes, patientIDs);
--- phasing/pacbio_whatshap.py
+++ phasing/pacbio_whatshap.py
@@ -1,20 +1,24 @@
-import subprocess as sp
+""" Script to run Whatshap on all patient IDs without indel flag using multiprocessing
+"""
 
-""" Script to run Whatshap with indels flag for Pacbio bams on all patient IDs """
+import subprocess as sp
+import multiprocessing as mp
 
 patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389", "1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
-for ID in patientID:
-
-    for i in range(1,23):
+def whatshap(ID):
+    for i in range(1, 23):
         vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-        vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
-        vcf_filename += str(i) + ".vcf.gz"
+        vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID "_chr" + str(num) + ".vcf.gz";
         bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/PacbioHg38Bams/";
         bam_filename += ID + "_edit_sorted.bam";
         command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
         command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
-        command += " --indels -o " + ID + "/" + ID + "_chr" + str(i) + "_phased.vcf ";
+        command += " -o " + ID + "_no_indels/" + ID + "_chr" + str(num) + "_phased.vcf ";
         command += vcf_filename + " " + bam_filename;
         sp.call(command, shell=True);
-        print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
+        print("======Sucessfully ran whatshap for" + ID + " on chromosome " + str(num));
+
+if __name__ == '__main__':
+  pool = mp.Pool(processes=5);
+  pool.map(whatshap, patientID);
