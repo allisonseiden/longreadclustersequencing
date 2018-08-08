d0f434d65cb66338e3b36a96f9828f3b9b78150b
Allison Seiden <allisonseiden@macbook-pro-2.wireless.mountsinai.org>
2018-08-08 13:13:53 -0400

    files for running second batch of illumina data from gmkf

--- phasing/illumina_whatshap_int2.py
+++ phasing/illumina_whatshap_int2.py
@@ -1,24 +1,32 @@
+import pandas as pd
 import subprocess as sp
 import multiprocessing as mp
 
 """ Script 2/2 to run Whatshap with indels flag using Illumina data for second
     5 patient IDs, uses multiprocessing """
 
-patientID = ["1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
+df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/data/Illumina_crams_batch2.txt', names=['ID']);
+patientID = df['ID'].tolist();
+
+# patientID = ["1-04460", "1-04537", "1-05443", "1-05673", "1-05846"];
 
 def illumina_whatshap(ID):
+    mkdir = 'mkdir ' + ID;
+    sp.call(mkdir, shell=True);
+    cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/IlluminaWhatshapVCFs/Batch2/' + ID;
+    sp.call(cd, shell=True);
     for i in range(1, 23):
         vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-        vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
-        vcf_filename += str(i) + ".vcf.gz"
-        bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-        bam_filename += "IlluminaHg38Bams/" + ID + ".hg38.dedup.clean.recal.cram";
+        vcf_filename += "IlluminaVCFs/Illumina_WGS_chr" + str(i) + ".vcf.gz";
+        bam_filename = "/sc/orga/projects/chdiTrios/GMKF_WGS_Trios_Dec_2017/CRAM/Batch2/";
+        bam_filename += ID + ".cram";
         command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
         command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
-        command += " --indels -o " + ID + "_illumina/" + ID + "_chr" + str(i) + "_phased.vcf ";
+        command += " --indels -o " + ID + "/" + ID + "_chr" + str(i) + "_phased.vcf ";
         command += vcf_filename + " " + bam_filename;
         sp.call(command, shell=True);
         print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
+    sp.call('cd ..', shell=True);
 
 if __name__ == '__main__':
   pool = mp.Pool(processes=5);
