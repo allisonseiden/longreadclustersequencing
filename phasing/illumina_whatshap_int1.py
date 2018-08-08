283332485c3082cbdd1b6675478ad47842cf2633
Allison Seiden <allisonseiden@macbook-pro-2.wireless.mountsinai.org>
2018-08-08 12:42:32 -0400

    running batch1 of illumina data

--- phasing/illumina_whatshap_int1.py
+++ phasing/illumina_whatshap_int1.py
@@ -7,25 +7,26 @@ import multiprocessing as mp
 
 df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/data/Illumina_crams_batch1.txt', names=['ID']);
 patientID = df['ID'].tolist();
-print(df);
-print(patientID);
 
 # patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389"];
-#
-# def illumina_whatshap(ID):
-#     for i in range(1, 23):
-#         vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-#         vcf_filename += "DNV_calls/VCF/TrioVCF/" + ID + "/" + ID + "_chr";
-#         vcf_filename += str(i) + ".vcf.gz"
-#         bam_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
-#         bam_filename += "IlluminaHg38Bams/" + ID + ".hg38.dedup.clean.recal.cram";
-#         command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
-#         command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
-#         command += " --indels -o " + ID + "_illumina/" + ID + "_chr" + str(i) + "_phased.vcf ";
-#         command += vcf_filename + " " + bam_filename;
-#         sp.call(command, shell=True);
-#         print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
-#
-# if __name__ == '__main__':
-#   pool = mp.Pool(processes=5);
-#   pool.map(illumina_whatshap, patientID);
+
+def illumina_whatshap(ID):
+    mkdir = 'mkdir ' + ID;
+    sp.call(mkdir, shell=True);
+    cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/' + ID;
+    sp.call(cd, shell=True);
+    for i in range(1, 23):
+        vcf_filename = "/sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/";
+        vcf_filename += "IlluminaVCFs/Illumina_WGS_chr" + str(i) + ".vcf.gz";
+        bam_filename = "/sc/orga/projects/chdiTrios/GMKF_WGS_Trios_Dec_2017/CRAM/Batch1/";
+        bam_filename += ID + ".cram";
+        command = "whatshap phase --sample=" + ID + " --ignore-read-groups";
+        command += " --reference /sc/orga/projects/chdiTrios/Felix/dbs/hg38.fa";
+        command += " --indels -o " + ID + "/" + ID + "_chr" + str(i) + "_phased.vcf ";
+        command += vcf_filename + " " + bam_filename;
+        sp.call(command, shell=True);
+        print("======Sucessfully ran whatshap for " + ID + " on chromosome " + str(i));
+
+if __name__ == '__main__':
+  pool = mp.Pool(processes=5);
+  pool.map(illumina_whatshap, patientID);
