6da14e2424fe2f370d827899fc3eef37c92e23f2
Allison Seiden <ahseiden@gmail.com>
2018-08-09 13:24:07 -0400

    troubleshooting

--- phasing/split_trios.py
+++ phasing/split_trios.py
@@ -34,9 +34,9 @@ def split_vcf(num):
     else:
         cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/';
         cd += 'GMKF_TrioVCFs/Batch3';
-    # sp.call(cd, shell=True);
+    sp.call(cd, shell=True);
     sp.call(command, shell=True);
-    # sp.call(index, shell=True);
+    sp.call(index, shell=True);
 
 
 if __name__ == '__main__':
