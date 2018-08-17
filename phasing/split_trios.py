a6213852dff6d2768dd6392c5b2d2e0fddee693c
frichter <frichter91@gmail.com>
2018-08-17 10:09:32 -0400

    updated split trios script to check if tabix was made

--- phasing/split_trios.py
+++ phasing/split_trios.py
@@ -14,7 +14,7 @@ module load python/3.5.0 py_packages/3.5
 cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/GMKF_TrioVCFs/
 python3 ~/longreadclustersequencing/phasing/split_trios.py
 
-One done, check that last line in every file is the same
+Once done, check that last line in every file is the same
 
 """
 
@@ -25,43 +25,46 @@ import multiprocessing as mp
 import os
 
 
-# batch1_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/data/Illumina_crams_batch1.txt', names=['ID']);
+# batch_prefix = '/hpc/users/seidea02/longreadclustersequencing/data/Illumina_'
+# batch1_df = pd.read_table(batch_prefix + 'crams_batch1.txt', names=['ID']);
 # batch1 = batch1_df['ID'].tolist();
-# 
-# batch2_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/data/Illumina_crams_batch2.txt', names=['ID']);
+#
+# batch2_df = pd.read_table(batch_prefix + 'crams_batch2.txt', names=['ID']);
 # batch2 = batch2_df['ID'].tolist();
-# 
-# batch3_df = pd.read_table('/hpc/users/seidea02/longreadclustersequencing/data/Illumina_crams_batch3.txt', names=['ID']);
+#
+# batch3_df = pd.read_table(batch_prefix + 'crams_batch3.txt', names=['ID']);
 # batch3 = batch3_df['ID'].tolist();
 
-trio_df = pd.read_table('/sc/orga/projects/chdiTrios/GMKF_WGS_Trios_Dec_2017/GMKF_Seidman_CongenitalHeartDisease_WGS.ped',
-                        names = ['Fam_ID', 'Child', 'Father', 'Mother'], sep='\t', comment='#');
 
-length = trio_df.shape[0];
 def split_vcf(num):
+    """Split VCF into trios to avoid whatshap memory issues."""
     kiddo = trio_df.loc[num, 'Child'];
     dad = trio_df.loc[num, 'Father'];
     mom = trio_df.loc[num, 'Mother'];
     print(kiddo)
-    if os.path.exists(kiddo + '_trio.vcf.gz'):
+    vcf_exists = os.path.exists(kiddo + '_trio.vcf.gz')
+    tbx_exists = os.path.exists(kiddo + '_trio.vcf.gz.tbi')
+    # first check if VCF created but not indexed (likely due to crash)
+    if vcf_exists and not tbx_exists:
+        print('Deleting the following file: ' + kiddo + '_trio.vcf.gz')
+        os.remove(kiddo + '_trio.vcf.gz')
+    # if BOTH VCF and tabix were created, skip and move on to next one
+    if vcf_exists and tbx_exists:
+        print('Already done with ' + kiddo)
         return kiddo
     command = 'bcftools view -s ' + kiddo + ',' + mom + ',' + dad;
     command += ' -O z -o ' + kiddo + '_trio.vcf.gz /sc/orga/projects/';
     command += 'chdiTrios/GMKF_WGS_Trios_Dec_2017/';
     command += 'GMKF_Seidman_CongenitalHeartDisease_WGS.vcf.gz';
     index = 'tabix -p vcf ' + kiddo + '_trio.vcf.gz';
+    # cd = ('cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/' +
+    #       'PacbioProject/');
     # if kiddo in batch1:
-    #     # cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/';
-    #     # cd += 'GMKF_TrioVCFs/Batch1';
-    #     sp.call('cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/GMKF_TrioVCFs/Batch1', shell=True);
+    #     sp.call(cd + 'GMKF_TrioVCFs/Batch1', shell=True);
     # elif kiddo in batch2:
-    #     # cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/';
-    #     # cd += 'GMKF_TrioVCFs/Batch2';
-    #     sp.call('cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/GMKF_TrioVCFs/Batch2', shell=True);
+    #     sp.call(cd + 'GMKF_TrioVCFs/Batch2', shell=True);
     # else:
-    #     # cd = 'cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/';
-    #     # cd += 'GMKF_TrioVCFs/Batch3';
-    #     sp.call('cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/GMKF_TrioVCFs/Batch3', shell=True);
+    #     sp.call(cd + 'GMKF_TrioVCFs/Batch3', shell=True);
     # sp.call(cd, shell=True);
     sp.call(command, shell=True);
     sp.call(index, shell=True);
@@ -69,5 +72,10 @@ def split_vcf(num):
 
 
 if __name__ == '__main__':
+    trio_df = pd.read_table('/sc/orga/projects/chdiTrios/GMKF_WGS_Trios_Dec_' +
+                            '2017/GMKF_Seidman_CongenitalHeartDisease_WGS.ped',
+                            names=['Fam_ID', 'Child', 'Father', 'Mother'],
+                            sep='\t', comment='#');
+    length = trio_df.shape[0];
     pool = mp.Pool(processes=7);
     pool.map(split_vcf, range(length));
