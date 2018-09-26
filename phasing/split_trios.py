37ce064b17fe42a9cb07b9582a5c1821fa4cc26c
frichter <frichter91@gmail.com>
2018-09-26 12:54:41 -0400

    phasing complete check

--- phasing/split_trios.py
+++ phasing/split_trios.py
@@ -52,7 +52,7 @@ def split_vcf(num):
     dad = trio_df.loc[num, 'Father']
     mom = trio_df.loc[num, 'Mother']
     vcf_exists = os.path.exists(kiddo + '_trio.vcf.gz')
-    vcf_done = os.path.exists('done_2018_09_26' + kiddo + '_trio.vcf.gz')
+    vcf_done = os.path.exists('done_2018_09_26/' + kiddo + '_trio.vcf.gz')
     """
     tbx_exists = os.path.exists(kiddo + '_trio.vcf.gz.tbi')
     # check if VCF created but not indexed (e.g., due to crash)
