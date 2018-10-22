9955ebbc183ed923eb87fe88d96079353c549abb
frichter <frichter91@gmail.com>
2018-10-22 11:26:08 -0700

    updated to only take completed VCFs

--- phasing/get_gtf.py
+++ phasing/get_gtf.py
@@ -23,28 +23,32 @@ python3 ~/longreadclustersequencing/phasing/get_gtf.py
 import subprocess as sp
 import multiprocessing as mp
 import glob
+import os
 
 # Script to get gtf files for all chromosomes for all IDs using multiprocessing
 
-patientID = ["1-00801", "1-01019", "1-03897", "1-04190", "1-04389",
-             "1-04460", "1-04537", "1-05673", "1-05846"]
+patientID = ['1-00801', '1-01019', '1-03897', '1-04190', '1-04389',
+             '1-04460', '1-04537', '1-05673', '1-05846']
 
 
 def get_gtf(ID):
-    """Run Whatshap's gtf function to get contiguously phased variants."""
+    """Run Whatshap's gtf function to get contiguously phased variants.
+
+    Original function for PacBio + Illumina data that Allison ran.
+    """
     for i in range(1, 23):
-        command = ("whatshap stats --gtf=" + ID + "_chr" + str(i) +
-                   "_phased.gtf " + ID + "_no_indels/" + ID + "_chr"
-                   + str(i) + "_phased.vcf")
+        command = ('whatshap stats --gtf=' + ID + '_chr' + str(i) +
+                   '_phased.gtf ' + ID + '_no_indels/' + ID + '_chr'
+                   + str(i) + '_phased.vcf')
         sp.call(command, shell=True)
-        print("Created gtf for " + ID + " chromosome " + str(i))
+        print('Created gtf for ' + ID + ' chromosome ' + str(i))
 
 
 def get_ilmn_vcf_list():
     """Get a list of VCFs that have been phased."""
     id_file_list = []
     # '1',
-    for batch_i in ['2', '3']:
+    for batch_i in ['1', '2', '3']:
         batch_f = 'Batch{}/*'.format(batch_i)
         id_file_list.extend([i for i in glob.iglob(batch_f)])
     ilmn_vcf_list = []
@@ -55,10 +59,13 @@ def get_ilmn_vcf_list():
 
 def get_gtf_ilmn(vcf):
     """Run Whatshap gtf for contiguously phased variants in Illumina data."""
-    gtf_cmd = "time whatshap stats --gtf={}.gtf {}".format(vcf[:-4], vcf)
-    print(gtf_cmd)
-    sp.call(gtf_cmd, shell=True)
-    print("Created gtf for " + vcf)
+    gtf_cmd = 'time whatshap stats --gtf={}.gtf {}'.format(vcf[:-4], vcf)
+    if not os.path.exists(vcf[:-4] + '.gtf'):
+        print(gtf_cmd)
+        sp.call(gtf_cmd, shell=True)
+        print('Created gtf for ' + vcf)
+    else:
+        print('GTF already done for ' + vcf)
 
 
 if __name__ == '__main__':
