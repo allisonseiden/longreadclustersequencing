cf59b6b713171a3442d2db12455eb3e3bd3d458d
frichter <frichter91@gmail.com>
2018-11-03 19:29:28 -0400

    whatshap job update

--- phasing/whatshap_bsub.sh
+++ phasing/whatshap_bsub.sh
@@ -3,10 +3,10 @@
 #BUSB -n 2
 #BSUB -R "rusage[mem=50000]"
 #BSUB -P acc_schade01a
-#BSUB -J whatshap_b1_11_02
+#BSUB -J whatshap_b1_11_03
 #BSUB -m mothra
-#BSUB -o whatshap_b1_11_02.stdout
-#BSUB -e whatshap_b1_11_02.stderr
+#BSUB -o whatshap_b1_11_03.stdout
+#BSUB -e whatshap_b1_11_03.stderr
 
 
 # submit with this command: 
@@ -19,5 +19,5 @@ module load samtools/1.8 bcftools/1.7 tabix
 module load python/3.5.0 py_packages/3.5
 source venv_phasing/bin/activate
 cd /sc/orga/projects/chdiTrios/WGS_Combined_2017/PacbioProject/\
-IlluminaWhatshapVCFs/Batch3/
-python3 ~/longreadclustersequencing/phasing/illumina_whatshap_int1.py --batch 3
+IlluminaWhatshapVCFs/Batch1/
+python3 ~/longreadclustersequencing/phasing/illumina_whatshap_int1.py --batch 1
